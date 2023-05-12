13ecefc584b65fa52eb08fde20c08253364f938a
isha-web1 <isha.web1@gmail.com>
2023-05-12 20:38:55 +0600

    delete oparation added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -45,6 +45,14 @@ async function run() {
       const result = await coffeeCollection.insertOne(newCoffee)
       res.send(result)
       
+    })
+
+
+    app.delete('/coffee/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result)
     })
 
 
